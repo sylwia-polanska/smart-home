3d2b811b1ddab2f8c6cc75030f2fa0618e30d769
sylwia-staszak <s.staszak13@gmail.com>
2022-02-19 14:36:02 +0100

    layout changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,71 +1,98 @@
 <template>
-  <div>
-    <div class="devicesList">
-      <h1>Name</h1>
-      <ul id="devicesName">
-        <li>a</li>
-      </ul>
-    </div>
-    <div class="devicesList">
-      <h1>Type</h1>
-      <ul id="devicesType">
-        <li>a</li>
-      </ul>
-    </div>
-    <div class="devicesList">
-      <h1>State</h1>
-      <ul id="devicesConnectionState">
-        <li>a</li>
-      </ul>
-    </div>
-  </div>
-    
+  <div id="mainTemplateContainer">
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>State</th>
+      </tr>
+      <tr v-for="device in mockedDevices.allDevices" :key="device.id">
+        <td>{{ device.name }}</td>
+        <td>{{ device.type }}</td>
+        <td>{{ device.connectionState }}</td>
+      </tr>
+    </table>    
+  </div>    
 </template>
 
 <script>
+export default{
+  data() {
+    return {
+      mockedDevices: {
+        allDevices: [],
+        smartBulbs: [
+          {
+            type: 'bulb',
+            id: '45255',
+            name: 'bulb1',
+            connectionState: 'connected', 
+            isTurnedOn: true,
+            brightness: 92,
+            color: 'blue'
+          },
+          {
+            type: 'bulb',
+            id: '23423',
+            name: 'bulb2',
+            connectionState: 'connected', 
+            isTurnedOn: true,
+            brightness: 90,
+            color: 'green'
+          }
+        ],
+        smartOutlets: [
+          {
+            type: 'outlet',
+            id: '34535',
+            name: 'outlet1',
+            connectionState: 'disconnected',
+            isTurnedOn: 'false',
+            powerConsumption: 20,
+          }
+        ],
+        SmartTemperatureSensor: [
+          {
+            type: 'temperatureSensor',
+            id: '34342',
+            name: 'temperaturesensor',
+            connectionState: 'poorConnection',
+            temperature: 25,
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    mixAllDevicesInOneArray() {
+      this.mockedDevices.allDevices.push(...this.mockedDevices.smartBulbs);
+      this.mockedDevices.allDevices.push(...this.mockedDevices.smartOutlets);
+      this.mockedDevices.allDevices.push(...this.mockedDevices.SmartTemperatureSensor);
+    }
+  },
+  mounted() {
+    this.mixAllDevicesInOneArray();
+  }
+}
 
 </script>
 
 <style scoped>
-  .devicesList {
-    height: 400px;
-    border: solid 1px black;
-    border-left: none;
-    display: inline-block;
-    list-style-type: none;
-    margin-top: 250px
-  }
-
-  .devicesList:first-of-type {
-    border-left: solid 1px black;
-  }
-
-  #devicesType, #devicesConnectionState{
-    width: 80px;
-  }
-
-  #devicesName {
-    width: 120px
-  }
-
-  h1 {
-    font-size: 20px;
-    margin-top: 15px;
-    display: inline-block;
-  }
-
-  ul {
-    margin: 0;
-    list-style-type: none;
-    padding-left: 10px;
-    text-align: left;
-    text-transform: capitalize;
-    font-size: 15px;
-  }
-
-  #devicesName li {
-    cursor: pointer;
-  }
-
-
+#mainTemplateContainer table {
+  margin: auto;
+  margin-top: 100px;
+  border-spacing: 0;
+}
+#mainTemplateContainer table td,
+#mainTemplateContainer table th {
+  padding: 10px;
+}
+#mainTemplateContainer table th {
+  border-bottom: 1px solid black;
+}
+#mainTemplateContainer table tr:hover td {
+  background-color: black;
+  color: white;
+  cursor: pointer;
+}
 </style>
